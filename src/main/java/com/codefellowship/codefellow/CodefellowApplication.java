e7109a58045fe0c6bd355766e1bf87b77b843b8b
FabianB14 <32110808+FabianB14@users.noreply.github.com>
2019-08-27 10:45:58 -0700

    starter up

---
+++ src/main/java/com/codefellowship/codefellow/CodefellowApplication.java
@@ -0,0 +1,13 @@
+package com.codefellowship.codefellow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodefellowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodefellowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codefellowship/codefellow/CodefellowApplicationTests.java
@@ -0,0 +1,16 @@
+package com.codefellowship.codefellow;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CodefellowApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
